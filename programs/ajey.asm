050054caf46820d81866c7f998e1a81d721c7c84
AjeyBharadwaj <chi.ajeyanantharamaiah@gmail.com>
2016-06-24 01:10:40 +0530

    With Colour Letters

--- programs/ajey.asm
+++ programs/ajey.asm
@@ -1,5 +1,5 @@
 ; ------------------------------------------------------------------
-; MikeOS Text Editor
+; MikeOS Text Printer 
 ; ------------------------------------------------------------------
 
 
@@ -9,10 +9,37 @@
 
 
 start:
+    mov si, txt_title_msg
+    call os_print_string
+    call update_colour
+    ret
 
+update_colour:
+    pusha
+    call get_random_colour
+    call os_draw_background
+    mov si, txt_title_msg
+    mov dh, 1 
+    mov dl, 0
+    call os_move_cursor
+    call os_print_string
+    call os_wait_for_key
+    jmp update_colour
+    popa
+    ret
 
+get_random_colour:
+    push ax
+    push bx
+ 
+    mov ax, 0
+    mov bx, 0fh
 
+    call os_get_random
 
+    pop ax
+    pop bx
+    ret
 
 txt_title_msg   db 'MikeOS Text Editor', 0
 
